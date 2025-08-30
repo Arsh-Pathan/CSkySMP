0fcd3529b65ff9a7259537266ed4b581aa836679
Arsh <mail.arsh.pathan@gmail.com>
2025-08-30 23:55:19 +0530

    Revert "Season9"

--- src/main/java/io/arsh/discord/utils/Emoji.java
+++
@@ -1,6 +0,0 @@
-package io.arsh.discord.utils;
-
-public class Emoji {
-    public final static net.dv8tion.jda.api.entities.emoji.Emoji CORRECT = net.dv8tion.jda.api.entities.emoji.Emoji.fromCustom("Correct", 1407042696089964674L, false);
-    public final static net.dv8tion.jda.api.entities.emoji.Emoji WRONG = net.dv8tion.jda.api.entities.emoji.Emoji.fromCustom("Wrong", 1407042700296851576L, false);
-}
